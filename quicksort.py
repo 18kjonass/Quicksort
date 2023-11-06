1cabb31d4c1f3fad681dd9bb0ac2ce2db43ac975
18kjonass <112894153+18kjonass@users.noreply.github.com>
2023-11-06 15:16:17 +0000

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,25 @@
+nums = [88,46,25,11,18,12,22]
+
+
+def Quicksort(nums):
+    if len(nums) <= 1:
+        return nums
+    else:
+        left_list = []
+        middle_list = []
+        right_list = []
+
+        pivot = nums[-1]
+        x = 0
+        for x in nums:
+            if pivot > x:
+                left_list.append(x)
+            elif pivot < x:
+                right_list.append(x)
+            else:
+                middle_list.append(x)
+        return Quicksort(left_list)+middle_list+Quicksort(right_list)
+        
+        
+    
+    
